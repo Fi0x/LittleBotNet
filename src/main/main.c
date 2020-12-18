23098f925755cbd0ebd1530b3581b4c6d14d044a
Felix Pechtl <fp-183054@hs-weingarten.de>
2020-12-18 16:53:07 +0100

    Initial Commit
    Signed-off-by: Felix Pechtl <fp-183054@hs-weingarten.de>

---
+++ src/main/main.c
@@ -0,0 +1,10 @@
+//
+// Created by fi0x on 18.12.20.
+//
+
+#include "main.h"
+#include <stdio.h>
+
+int main() {
+    printf("Test");
+}
---
+++ src/main/main.h
@@ -0,0 +1,8 @@
+//
+// Created by fi0x on 18.12.20.
+//
+
+#ifndef LITTLEBOTNET_MAIN_H
+#define LITTLEBOTNET_MAIN_H
+
+#endif //LITTLEBOTNET_MAIN_H
